47dd7ff404a26c28fb21972728adb261fee9e468
jianghong.li <ljc555@gmail.com>
2017-11-15 19:21:57 +0800

    开始创建工程

---
+++ sprinb-boot-demo/src/main/java/com/ljh/SprinbBootDemoApplication.java
@@ -0,0 +1,12 @@
+package com.ljh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinbBootDemoApplication.class, args);
+	}
+}
